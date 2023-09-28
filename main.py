cdfc35d01496bc6338e909a3becca0863058b9a0
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-28 09:23:54 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+class TravelGuide:
+    def __init__(self, user_preferences):
+        self.user_preferences = user_preferences
+
+    def generate_itinerary(self):
+        # Use OpenAI GPT to understand the user's preferences and create a personalized itinerary
+        pass
